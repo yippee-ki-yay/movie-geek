316d5f080db77ec83b34841bb701e6779cf20669
nenad <nesa993@gmail.com>
2016-12-26 21:14:20 +0100

    added seperate messanger integration

--- main.js
+++ main.js
@@ -4,103 +4,43 @@ if (process.env.NODE_ENV !== 'production'){
   require('dotenv').load()
 }
 
-let Wit = require('node-wit').Wit;
-let interactive = require('node-wit').interactive;
 let rp = require('request-promise');
-let express = require('express');
+const http = require('http');
+const FbBot = require('messenger-bot');
+const movie = require('./movie');
 
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 const FB_APP_SECRET = process.env.FB_APP_SECRET;
 
-const accessToken = process.env.ACCESS_TOKEN;
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
-function getMovieData(movie) {
-  let queryString = `http://www.omdbapi.com/?t=${escape(movie)}&y=&plot=full&r=json`;
+let bot = new FbBot({
+  token: FB_PAGE_TOKEN,
+  verify: FB_VERIFY_TOKEN,
+  app_secret: FB_APP_SECRET
+});
 
-  return rp(queryString);
-}
+bot.on('error', (err) => {
+  console.log(err.message);
+});
 
-function formatAnswer(movieData, intent) {
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text;
 
-  switch (intent) {
-    case 'relase_date':
-      return `The movie was relased ${movieData.Released}`
-    break;
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err;
 
-    case 'rating':
-      return `The movie has a imdb rating of ${movieData.imdbRating}`
-    break;
+    reply({ text }, (err) => {
+      if (err) throw err;
 
-    default:
-      return `Doesn't seem like anything to me`
-  }
-}
-
-const firstEntityValue = (entities, entity) => {
-  const val = entities && entities[entity] &&
-    Array.isArray(entities[entity]) &&
-    entities[entity].length > 0 &&
-    entities[entity][0].value
-  ;
-  if (!val) {
-    return null;
-  }
-  return typeof val === 'object' ? val.value : val;
-};
-
-
-const actions = {
-  send(request, response) {
-
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    console.log('sending...', JSON.stringify(response));
-  },
-  getResponse({context, entities}) {
-    return new Promise(function(resolve, reject) {
-
-      const movie = firstEntityValue(entities, "movie");
-
-      if(movie) {
-        getMovieData(movie)
-        .then(data => {
-
-          let movieData = JSON.parse(data);
-
-          let intent = null;
-
-          if(entities.intent[0]) {
-            intent = entities.intent[0].value;
-          }
-
-          context.response  = formatAnswer(movieData, intent);
-
-          return resolve(context);
-        })
-        .catch(err => {
-
-          return resolve(context);
-        })
-      } else {
-        return resolve(context);
-      }
-
-    });
-  }
-};
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
+    })
+  })
+});
 
 // server setup
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-
-
-app.get('/', (req, res) => {
-  res.send("Hello this is your friendly bot, definitely not trying to kill all humans");
+http.createServer(bot.middleware()).listen(PORT, () => {
+  console.log(`Echo bot server running at port ${PORT}.`);
 });
-
-
-
-const client = new Wit({accessToken, actions});
-interactive(client);
